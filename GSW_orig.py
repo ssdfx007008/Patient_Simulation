637b721d759d53081f734eb946e5976693898403
ssdfx007008 <gcui3@illinois.edu>
2016-06-19 01:16:52 -0500

    The first version

---
+++ GSW_orig.py
@@ -0,0 +1,92 @@
+import sys
+from PyQt4 import  QtGui
+from PyQt4.QtCore import *
+from PyQt4 import QtCore
+from patient import Patient
+
+class GSW(QtGui.QWidget):
+
+
+    def __init__(self):
+        super(GSW,self).__init__()
+        self.patient=Patient(self)
+        self.initUI()
+
+    # the GUI contains two major layout, patient_states and operation_options
+    # the main thread here is the GUI
+    # the second thread is the patient
+    # we grab all the data we have from the second thread and display it on GUI(main thread)
+    # the GUI interact with second thread using signals
+    def initUI(self):
+        self.mainlayout=QtGui.QVBoxLayout()
+        self.patient_states=QtGui.QBoxLayout(0x1)
+        self.mainlayout.addLayout(self.patient_states)
+        self.add_States_to_layout()
+        self.operation_options=QtGui.QBoxLayout(0x1)
+        self.mainlayout.addLayout(self.operation_options)
+        self.setLayout(self.mainlayout)
+        self.setWindowTitle('GSW')
+        self.connect(self.patient,SIGNAL("patient_state_changed"),self.State_changed)
+        self.connect(self.patient,SIGNAL("Patient Cured"),self.Patient_Cured)
+        self.emit(SIGNAL("Initialize patient"))
+        self.show()
+
+    def State_changed(self):
+        print("show changed states on GUI")
+        self.add_Button_to_layout()
+        self.set_States_to_layout()
+
+
+    def set_States_to_layout(self):
+        print("start set_States_to_layout")
+        self.HR.setPlainText("HR"+"\r\n"+str(self.patient.states[0]))
+        self.BPH.setPlainText("BPH"+"\r\n"+str(self.patient.states[1]))
+        self.BPL.setPlainText("BPL"+"\r\n"+str(self.patient.states[2]))
+        self.RR.setPlainText("RR"+"\r\n"+str(self.patient.states[3]))
+        self.SatsPercentage.setPlainText("SatsPercentage"+"\r\n"+str(self.patient.states[4]))
+        self.SatsDescription.setPlainText("SatsDescription"+"\r\n"+str(self.patient.states[5]))
+        print("finished set_States_to_layout")
+
+
+    def add_States_to_layout(self):
+        print("start add_States_to_layout")
+        self.HR = QtGui.QPlainTextEdit("HR")
+        self.BPH = QtGui.QPlainTextEdit("BPH")
+        self.BPL = QtGui.QPlainTextEdit("BPL")
+        self.RR = QtGui.QPlainTextEdit("RR")
+        self.SatsPercentage = QtGui.QPlainTextEdit("SatsPercentage")
+        self.SatsDescription = QtGui.QPlainTextEdit("SatsDescription")
+        self.patient_states.addWidget(self.HR )
+        self.patient_states.addWidget(self.BPH)
+        self.patient_states.addWidget(self.BPL)
+        self.patient_states.addWidget(self.RR)
+        self.patient_states.addWidget(self.SatsPercentage)
+        self.patient_states.addWidget(self.SatsDescription)
+
+    def add_Button_to_layout(self):
+        i=0
+        print("starting  loop of adding buttons")
+        for i in reversed(range(self.operation_options.count())):
+            self.operation_options.itemAt(i).widget().setParent(None)
+        if self.patient.next_states!=[] :
+            for name in self.patient.next_states:
+                print("adding Buttons")
+                new_buttons=QtGui.QPushButton(name)
+                self.operation_options.addWidget(new_buttons)
+                new_buttons.clicked.connect(self.button_clicked)
+        print("finished adding")
+
+    def button_clicked(self):
+        self.emit(SIGNAL(self.sender().text()))
+
+    def Patient_Cured(self):
+        reply = QtGui.QMessageBox.question(self, 'Message',"Patient is cured")
+        self.past_treatment.setText(self.past_treatment.toPlainText()+"\r\n"+self.sender().text())
+        self.mainlayout.patientstate.adjustSize()
+
+        
+if __name__ == '__main__':
+    
+    app = QtGui.QApplication(sys.argv)
+    ex = GSW()
+    sys.exit(app.exec_())
---
+++ patient.py
@@ -0,0 +1,176 @@
+import sys
+from PyQt4 import QtGui
+from PyQt4.QtCore import *
+from PyQt4 import QtCore
+import time
+
+
+class Patient(QThread):
+    # the operation list contains all the buttons we  needed in the main window
+    # the index will be saved by order, the value will be recorded in states
+    # states[0]=HR
+    # states[1]=BPH
+    # states[2]=BPL
+    # states[3]=RR
+    # states[4]=SatsPercentage
+    # states[5]=SatsDescription
+
+    # next_states[] passes all the states available to the main GUI
+    # then it creates a dynamic table for the buttons
+    # after specify all the available next states it should connect all the available signals to the next state functions
+
+
+    def __init__(self,parent):
+        super(Patient, self).__init__()
+        self.parent=parent
+        # when initializing the patient thread, we need to start the thread from the main thread, which is just before displaying the main GUI
+        self.connect(parent,SIGNAL("Initialize patient"),self.start)
+
+    def run(self):
+        print("Initializing patient")
+        self.states=[]
+        self.states.append(130)
+        self.states.append(85)
+        self.states.append(52)
+        self.states.append(29)
+        self.states.append(89)
+        self.states.append( "RA")
+        self.next_states=[]
+        self.On_state_changed()
+        self.GSW_Prehospital()
+
+
+        # every time the patient state changed, there will be a signal generated to tell the main gui
+        # to read the states again and generate a new state display, then a new button list
+
+    def On_state_changed(self):
+        print("\r\n\r\nState Changed")
+        self.emit(SIGNAL("patient_state_changed"))
+
+    def GSW_Prehospital(self):
+        print("GSW_Prehospital")
+        self.states[0] = 130
+        self.states[1] = 85
+        self.states[2] = 52
+        self.states[3] = 29
+        self.states[4] = 89
+        self.states[5] = "RA"
+        self.On_state_changed()
+        start=time.time()
+        a=0
+        while time.time()-start < 3 :
+            a=a+1
+        self.Initial_Trauma_Bay()
+
+    def Initial_Trauma_Bay(self):
+        print("Initializing TraumaBay")
+        self.states[0] = 134
+        self.states[1] = 89
+        self.states[2] = 50
+        self.states[3] = 30
+        self.states[4] = 90
+        self.states[5] = "NRB"
+        self.next_states=[]
+        self.next_states.append("Needle decompression")
+        self.connect(self.parent, SIGNAL("Needle decompression"), self.Needle_Decompression)
+        self.next_states.append("Place Chest Tube")
+        self.connect(self.parent, SIGNAL("Place Chest Tube"), self.Chest_tube_placed)
+        self.new_thread = No_chest_tube_300s_timer()
+        # the timer permission gives the right for the timer to change the state, if 0 then the timer is not allowed to change the patient state
+        self.timer_permission = 1
+        self.new_thread.start()
+        self.On_state_changed()
+
+
+    def Needle_Decompression(self):
+        self.states[0] = 88
+        self.states[1] = 108
+        self.states[2] = 54
+        self.states[3] = 12
+        self.states[4] = 98
+        self.states[5] = "NRB"
+        self.new_thread = No_chest_tube_300s_timer()
+        self.connect(self.new_thread, SIGNAL("No_chest_tube_placed_300s()"), self.No_chest_tube_300s)
+        self.new_thread.start()
+        self.next_states.clear()
+        self.next_states.append("Place Chest Tube")
+        self.connect(self.parent,SIGNAL("Place Chest Tube"),self.Chest_tube_placed)
+        self.On_state_changed()
+
+    def Chest_tube_placed(self):
+        print("Chest_tube_placed")
+        self.states[0] = 87
+        self.states[1] = 108
+        self.states[2] = 54
+        self.states[3] = 12
+        self.states[4] = 98
+        self.states[5] = "NRB"
+        self.timer_permission=0
+        self.next_states=[]
+        self.On_state_changed()
+        self.emit(SIGNAL("Patient Cured"))
+
+
+    def No_chest_tube_300s(self):
+        print("No_chest_tube_300s")
+        self.states[0] = 160
+        self.states[1] = 55
+        self.states[2] = 20
+        self.states[3] = 40
+        self.states[4] = 82
+        self.states[5] = "NRB"
+        self.new_thread = No_chest_tube_placed_180s_timer(self)
+        self.new_thread.start()
+        self.connect(self.new_thread, SIGNAL("No_chest_tube_placed"), self.No_chest_tube_placed)
+        self.connect(self.parent, SIGNAL("Chest_tube_placed"), self.Chest_tube_placed)
+        self.next_states=[]
+        self.next_states.append("Chest Tube Placed")
+        self.On_state_changed()
+
+    def No_chest_tube_placed(self):
+        if self.timer_permission==1:
+            print("No_chest_tube_placed")
+            self.states[0] = 0
+            self.states[1] = 0
+            self.states[2] = 0
+            self.states[3] = 0
+            self.states[4] = 0
+            self.states[5] = "None"
+            self.On_state_changed()
+
+
+class No_chest_tube_placed_180s_timer(QThread):
+    def __initial__(self, parent):
+        super(No_chest_tube_placed_180s_timer, self).__init__()
+        self.new_signal=QtCore.SIGNAL("No_chest_tube_placed_180s")
+
+    def run(self):
+        print("in thread\n")
+        start = time.time()
+        a = 0
+        while (time.time() - start < 1.8):
+            a = a + 1
+        self.emit(self.new_signal)
+
+
+
+
+class No_chest_tube_300s_timer(QtCore.QThread):
+    def __init__(self):
+        super(No_chest_tube_300s_timer, self).__init__()
+        self.new_signal=QtCore.SIGNAL("No_chest_tube_placed_300s")
+    def run(self):
+        print("in thread\n")
+        start=time.time()
+        a=0
+        while(time.time()-start<3.0):
+            a=a+1
+        self.emit(self.new_signal)
+
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    ex = Patient(app)
+
+    sys.exit(app.exec_())
